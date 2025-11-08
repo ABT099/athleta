b8bd4d25eb1381c9807b378830dee7e08b48941b
Abdullah Towait <abdo.formal99@gmail.com>
2025-11-08 23:21:20 +0300

    fixes

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,15 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
 
 async function bootstrap() {
-    
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-  });
-  const db = drizzle({ client: pool });
-
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
